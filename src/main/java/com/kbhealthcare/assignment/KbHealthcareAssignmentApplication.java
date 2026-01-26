7f6d499ed5bd76077289c5f5e0f09ceb019e6a7c
HJ Park <b2aconnn@gmail.com>
2026-01-26 13:05:40 +0900

    chore: 프로젝트 기본 구조 및 패키지 구성

---
+++ src/main/java/com/kbhealthcare/assignment/KbHealthcareAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.kbhealthcare.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbHealthcareAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbHealthcareAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kbhealthcare/assignment/KbHealthcareAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbhealthcare.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbHealthcareAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
