8abffc5fcf4a3c6e3f9e9e8212b0c212d7567d4d
HJ Park <b2aconnn@gmail.com>
2026-01-29 01:12:37 +0900

    chore: JPA 공통 설정 및 BaseEntity 추가

---
+++ src/main/java/com/kbhealthcare/assignment/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.kbhealthcare.assignment.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
---
+++ src/main/java/com/kbhealthcare/assignment/domain/common/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.kbhealthcare.assignment.domain.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private ZonedDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private ZonedDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        ZonedDateTime now = ZonedDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = ZonedDateTime.now();
+    }
+}
